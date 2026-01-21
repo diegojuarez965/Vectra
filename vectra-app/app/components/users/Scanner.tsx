be29e9e8f7348ed769f7a3da2cf22eb789133c5f
Diego Juarez <diegohernanjuarez965@gmail.com>
2026-01-21 16:16:40 -0300

    Test CPU

--- vectra-app/app/components/users/Scanner.tsx
+++ vectra-app/app/components/users/Scanner.tsx
@@ -59,7 +59,7 @@ export default function Scanner() {
           baseOptions: {
             modelAssetPath:
               "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task", // Cargamos el modelo
-            delegate: "GPU",
+            delegate: "CPU",
           },
           runningMode: "VIDEO",
           numPoses: 1,
